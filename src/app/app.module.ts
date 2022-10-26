5b7e949534ee5e9e768d3727cc4bab4089a73944
SebaChai <chaileluissebastian@gmail.com>
2022-10-26 10:39:28 -0300

    segunda prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { CentroComponent } from './centro/centro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CentroComponent
   ],
   imports: [
     BrowserModule,
